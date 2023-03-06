aac9e04a5b98349a6b5f9889f3a8cd35c2c09a04
2enny <lambeeniola02@gmail.com>
2023-03-06 10:06:16 -0800

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of apointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
